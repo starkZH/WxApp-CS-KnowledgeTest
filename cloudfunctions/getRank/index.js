64856b22e605689072302bed5501c04b8aac8431
starkZH <1091173854@qq.com>
2020-02-24 21:01:24 +0800

    完善获取排行榜函数：若还未排名则先排名再返回结果

--- cloudfunctions/getRank/index.js
+++ cloudfunctions/getRank/index.js
@@ -14,15 +14,26 @@ exports.main = async (event, context) => {
   let result={};
   const wxContext = cloud.getWXContext()
   let user_openid = wxContext.OPENID;
-  let limit = Number(event.limit);
+  //let limit = Number(event.limit);
   let data=[];
+  let exam_id = event.exam_id;
   await db.collection('answer_formal').field({
     user_id:true,time_cost:true,score:true,rank:true
   }).where({
-    exam_id:event.exam_id
-  }).limit(limit).orderBy('rank','asc').get().then((res)=>{
+    exam_id: exam_id
+  })
+  //.limit(limit)
+  .orderBy('rank','asc').get().then((res)=>{
     data=res.data;
   });
+  //尚未进行排名，则先排名后返回
+  if(data[0]&&!data[0].rank){
+    result = await cloud.callFunction({
+      name: 'calculateRank',
+      data: { exam_id }
+    });
+    return result.result;
+  }
   for(let val of data){
     try {
       await db.collection('sys_user').doc(val.user_id).get().then(res => {
