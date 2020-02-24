c38fbb6e1c4605440c36b1e562d1fc24b2d4b0ad
starkZH <1091173854@qq.com>
2020-02-24 22:22:39 +0800

    把小程序首页设置的图片id转换为链接

--- cloudfunctions/getHomeSettings/index.js
+++ cloudfunctions/getHomeSettings/index.js
@@ -12,6 +12,16 @@ exports.main = async (event, context) => {
   await db.collection('base_setting').orderBy('order','asc').get().then(res=>{
     result.data=res.data;
   })
-
+  let fileList = [];
+  for(let val of result.data){
+    fileList.push(val.img_id);
+  }
+  fileList = await cloud.getTempFileURL({
+    fileList: fileList,
+  });
+  fileList = fileList.fileList;
+  for (let i in fileList){
+    result.data[i].img_url = fileList[i].tempFileURL;
+  }
   return result;
 }
