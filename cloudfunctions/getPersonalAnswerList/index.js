69d4ad37c0f1ed15755a75ed8fa5208c0b1d0ab7
starkZH <1091173854@qq.com>
2020-03-10 19:01:36 +0800

    为答题列表每一项增加试题名称

--- cloudfunctions/getPersonalAnswerList/index.js
+++ cloudfunctions/getPersonalAnswerList/index.js
@@ -13,11 +13,23 @@ const _ = db.command;
 let table = { 1: "answer_formal", 2: "answer_simulation" };
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-  let result = {},arr=[];
+  let result = {}, arr = [];
+  var idSet = new Set();
   await db.collection(table[event.type]).field({answer:false}).where({ user_id: _.eq(wxContext.OPENID)}).get().then((res)=>{
     arr = res.data
+    for (var val of arr) {
+      idSet.add(val.exam_id);
+    }
   })
-
+  await db.collection('exam').where({ _id: _.in(Array.from(idSet)) }).get().then((res) => {
+    let obj = {};
+    for (var val of res.data) {
+      obj[val._id] = val.name;
+    }
+    for (var val of arr) {
+      val.exam_name = obj[val.exam_id];
+    }
+  })
   result.data = arr;
 
   return result;
